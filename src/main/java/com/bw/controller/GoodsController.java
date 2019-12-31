b93d2b748d7e6612ca19721fe851d1d62b24a07b
songxd <songxdqq.com>
2019-12-31 16:58:58 +0800

    测试第二次

--- src/main/java/com/bw/controller/GoodsController.java
+++ src/main/java/com/bw/controller/GoodsController.java
@@ -49,7 +49,7 @@ public class GoodsController {
 		modelAndView.setViewName("list");
 		
 		modelAndView.getModelMap().addAttribute("page", page);
-		
+		System.out.println();
 		return modelAndView;
 	}
 	
